0bfcefe54a798eeae4abc32da0e5edd4c96ac6bd
Nik Horbachov <nikcosmos@gmail.com>
2024-01-05 10:15:15 +0200

    feature: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="container max-w-5xl">
+      <div class="p-4 mt-5 ring-1 rounded-lg bg-white/70">
+         <h1 class="text-lg font-semibold">Calendar</h1>
+      </div>
+   </div>
+</template>
