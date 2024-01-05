c94b2769135cec03bb2978372f1cc6800127b375
Nik Horbachov <nikcosmos@gmail.com>
2024-01-05 10:01:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calendar Vue2
+
+### <span style = 'color:teal'>Node</span> - >=18.0.0
+
+```
+npm i
+npm run dev
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+   <div id="app">
+      <div class="text-red-500">Hello</div>
+   </div>
+</template>
