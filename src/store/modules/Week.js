827b96fcf154ea62e9a64acfc562c17f4d65b718
Nik Horbachov <nikcosmos@gmail.com>
2024-01-07 12:30:49 +0200

    refactor: moment.js => day.js

--- src/store/modules/Week.js
+++ src/store/modules/Week.js
@@ -1,8 +1,15 @@
-import moment from 'moment';
+import dayjs from 'dayjs';
+import isToday from 'dayjs/plugin/isToday';
+import isoWeek from 'dayjs/plugin/isoWeek';
+import toObject from 'dayjs/plugin/toObject';
+
+dayjs.extend(isoWeek);
+dayjs.extend(toObject);
+dayjs.extend(isToday);
 
 export default {
    state: {
-      day: moment().isoWeekday('Monday'),
+      day: dayjs().isoWeekday(1),
       week: [],
    },
    getters: {
@@ -19,34 +26,28 @@ export default {
          this.commit('setRooms');
       },
       nextWeek(state) {
-         state.day = moment(state.day).isoWeekday('Monday').add(1, 'w');
+         state.day = dayjs(state.day).isoWeekday(1).add(1, 'w');
          this.commit('setWeek');
       },
       prevWeek(state) {
-         state.day = moment(state.day).isoWeekday('Monday').subtract(1, 'w');
+         state.day = dayjs(state.day).isoWeekday(1).subtract(1, 'w');
          this.commit('setWeek');
       },
       returnToday(state) {
-         state.day = moment().isoWeekday('Monday');
+         state.day = dayjs().isoWeekday(1);
          this.commit('setWeek');
       },
    },
 };
+
 function createWeekArr(data = '') {
    const result = [];
    for (let i = 1; i < 8; i++) {
-      const day = moment(data).day(i).toObject();
-      const formatDay = moment(data).day(i).format('YYYY-MM-DD');
+      const formatDay = dayjs(data).day(i).format('YYYY-MM-DD');
       result.push({
          day: formatDay,
-         isToday: getIsToday(day),
+         isToday: dayjs(formatDay).isToday(),
       });
    }
    return result;
-}
-function getIsToday(day) {
-   const today = moment().toObject();
-   const isToday =
-      day.date === today.date && day.months === today.months && day.years === today.years;
-   return isToday;
 }
